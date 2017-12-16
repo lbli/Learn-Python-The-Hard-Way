ebd6b1007343c584276e6f0b4d62321db119e09c
lbli <877455661@qq.com>
2017-12-16 18:16:46 +0800

    Asking Questions

---
+++ ex11.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# Title: Asking Questions
+# Author : lbli
+
+print "How old are you?",
+age = raw_input ()
+print "How tall are you?",
+height = raw_input ()
+print "HOw much do you weight?",
+weight = raw_input ()
+
+print "So,you're %r old,%r tall and %r heavy." % (
+age, height, weight )
