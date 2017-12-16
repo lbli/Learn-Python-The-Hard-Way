f077fa04fc4390847f568c88475301307171b39b
lbli <877455661@qq.com>
2017-12-17 00:02:22 +0800

    Reading and Writing Files

---
+++ ex16.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# Title :Reading and Writing files
+# Author :lbli
+
+from sys import argv
+
+script,filename = argv
+
+print "We're going to erase %r." % filename
+print "If you don't wnat that,hit CTRL-C."
+print "If you do want to that hit RETURN"
+
+raw_input ( "?" )
+print "Openning the file..."
+target = open ( filename, 'w' )
+
+print "Truncating the file.Goodbye!"
+target.truncate ()
+
+print "NOw I'm going to ask you for three lines."
+
+line1 = raw_input ( "line 1: " )
+line2 = raw_input ( "line 2: " )
+line3 = raw_input ( "line 3: " )
+
+print "I'm going to write these to the file."
+
+target.write ( line1 )
+target.write ( "\n" )
+target.write ( line2 )
+target.write ( "\n" )
+target.write ( line3 )
+target.write ( "\n" )
+
+print "And finally.we close it."
+target.close ()
+
