cfbf8297ad7a72ba8d1008f7ce3f80474ff0095b
lbli <877455661@qq.com>
2017-12-16 19:32:08 +0800

    Parameters,Upacking,Variables

---
+++ ex13.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# Title : Parameters.Upacking,Variables
+# Author:lbli
+# remind :you should excute in this way: python ex12.py first 2nd 3rd
+
+
+from sys import argv
+
+script,first,second,third = argv
+
+
+print "The srcipt is called:",script
+print "Your first variable is :",first
+print "Your second variable is :",second
+print "Your third variable is :",third
