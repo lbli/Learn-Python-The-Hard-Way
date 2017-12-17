e31ef732878f8e558b309b9621afa7e7caa36085
lbli <877455661@qq.com>
2017-12-17 10:40:37 +0800

    Else and If

---
+++ ex30.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# Title : Else and If
+# Author : lbli
+
+people = 30
+cars = 40
+buses = 15
+
+if cars > people:
+    print "We should take the cars."
+elif cars < people:
+    print "We should not take the cars"
+else:
+    print "We can't decide."
+
+if buses > cars:
+    print "That's too many buses."
+elif buses < cars:
+    print "Maybe we could take the buses."
+else:
+    print "We still can't decide."
+
+if people > buses:
+    print "Alright, let's just take the buses."
+else:
+    print "Fine, let's stay home then."
+
+if people > cars and people < buses:
+    print "There are many Busses but few cars."
+elif people < cars and people > buses:
+    print "There are many cars but few busses."
+elif people > cars and people > buses:
+    print "There are few busses and cars."
+else:
+    print "There are many busses and cars."
